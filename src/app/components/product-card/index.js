2c598324349c14ad65950d9fefa4f55b58bd8f6c
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-07-27 18:13:30 +0700

    fetch allProducts and productDetails on action

---
+++ src/app/components/product-card/index.js
@@ -0,0 +1,44 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardTitle,
+  } from "@/components/ui/card"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+
+const ProductCard = ({item}) => {
+
+    const router = useRouter();
+
+    return (
+        <Card>
+            <CardContent>
+                <div className="w-full aspect-w-16 aspect-h-8 lg:h-80">
+                    <img
+                        src={item?.thumbnail} 
+                        alt={item?.title}
+                        className="w-full h-full object-cover object-top"
+                    />
+                </div>
+                <div className="p-6">
+                    <CardTitle className="text-lg font-bold text-gray-900">
+                        {item?.title}
+                    </CardTitle>
+                    <div className="mt-4 flex justify-between items-center flex-wrap gap-2">
+                        <p className="text-lg font-extrabold text-gray-800">
+                            {item?.price}
+                        </p>
+                        <div onClick={() => router.push(`/${item?.id}`)}>
+                            <Button>Details</Button>
+                        </div>
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default ProductCard
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
+import { fetchAllProducts } from "@/actions";
+import ProductCard from "./components/product-card";
 
+export default async function Home() {
 
-export default function Home() {
+  const getAllProducts = await fetchAllProducts();
+
+  console.log(getAllProducts);
+
   return (
     <div>
-      <h1>Nextjs-Auth</h1>
+      <h1>Shopping Cart</h1>
+      <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 max-w-6xl mx-auto p-2">
+        {
+          getAllProducts && getAllProducts.data && getAllProducts.data.length > 0 ? (
+            getAllProducts.data.map((productItem) => <ProductCard key={productItem.id} item={productItem}/>)
+          ) : null
+        }
+      </div>
     </div>
   );
 }
