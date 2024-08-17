5e2d5e2741ad7c1f109efc154702f7eb855696c6
nguyentuanhung123 <nguyentuanhung4529871036@gmail.com>
2024-08-18 02:13:04 +0700

    edit code on ReduxProvider and CommonLayout

--- src/components/common-layout/index.js
+++ src/components/common-layout/index.js
@@ -2,6 +2,7 @@ import Loading from "@/app/loading";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
+import Header from "../header";
 
 const { default: ReduxProvider } = require("@/provider");
 
@@ -10,7 +11,13 @@ async function CommonLayout({ children }) {
     const getSession = await auth();
 
     return (
-        <ReduxProvider getSession={getSession}>
+        // <ReduxProvider getSession={getSession}>
+        //     <Suspense fallback={<Loading />}>
+        //         {children}
+        //     </Suspense>
+        // </ReduxProvider>
+        <ReduxProvider>
+            <Header getSession={getSession} />
             <Suspense fallback={<Loading />}>
                 {children}
             </Suspense>
--- src/provider/index.js
+++ src/provider/index.js
@@ -6,16 +6,29 @@ import Header from '@/components/header'
 
 /**
  * Component này nằm trong CommonLayout.jsx
+ * Đoạn code lúc đầu
  */
 
-export default function ReduxProvider({ children, getSession }) {
+// export default function ReduxProvider({ children, getSession }) {
+//     return (
+//         <Provider store={store}>
+//             <Header getSession={getSession}/>
+//             {children}
+//         </Provider>
+//     )
+// }
+
+/**
+ * Đoạn code sửa lại
+ */
+export default function ReduxProvider({ children }) {
     return (
         <Provider store={store}>
-            <Header getSession={getSession}/>
             {children}
         </Provider>
     )
 }
+
 // export default function ReduxProvider({ children }) {
 //     return (
 //         <Provider store={store}>
